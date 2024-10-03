e46c267b5e5972585f9711b0bc423f5de2f27e07
Nicole <qlngncl@gmail.com>
2024-10-04 01:43:55 +0800

    home

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="/">Home</a>
+            <a className="nav-link" href="/#home">Home</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/#about">About</a>
