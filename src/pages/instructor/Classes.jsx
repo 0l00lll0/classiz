7724fd4d45b052c6ae746d169276b4307c817ce7
Nicole <qlngncl@gmail.com>
2024-10-03 15:46:13 +0800

    Fixed class card container size

--- src/pages/instructor/Classes.jsx
+++ src/pages/instructor/Classes.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import personIcon from '../../media/person-icon.svg'; // Adjust the path as necessary
-import '../../css/Classes.css'; // Ensure this file contains the CSS styles
+import personIcon from '../../media/person-icon.svg'; 
+import '../../css/Classes.css'; 
 
 const Classes = ({ classes, onCardClick }) => {
   return (
-    <div>
-      <h2>Class List</h2>
-      <div className="grid">
-        {classes.map((classItem, index) => (
-          <div
-            className="card"
-            key={index}
-            onClick={() => onCardClick(classItem)} // Add click event handler
-          >
-            <div className="card-content">
-              <h3 className="card-title">{classItem.name}</h3>
-              <hr />
-              <p>{classItem.description}</p> {/* Use description instead of code */}
-              <p className="card-text">
-                <img
-                  src={personIcon}
-                  alt="Students Icon"
-                  className="icon-image"
-                />
-                {classItem.enrollment} Students Enrolled
-              </p>
+    <div id='classes' className="container">
+      <main className="main-content">
+        <div className="classlist-card">
+          <h2 className="welcome-title">Classes</h2>
+        </div>
+
+        <div className="grid">
+          {classes.map((classItem, index) => (
+            <div
+              className="card"
+              key={index}
+              onClick={() => onCardClick(classItem)} 
+            >
+              <div className="card-content">
+                <h3 className="card-title">{classItem.name}</h3>
+                <hr />
+                <p>{classItem.description}</p>
+                <p className="card-text">
+                  <img
+                    src={personIcon}
+                    alt="Students Icon"
+                    className="icon-image"
+                  />
+                  {classItem.enrollment} Students Enrolled
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
@@ -38,11 +43,11 @@ Classes.propTypes = {
   classes: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired, // Ensure description is required
+      description: PropTypes.string.isRequired,
       enrollment: PropTypes.number.isRequired,
     })
   ).isRequired,
-  onCardClick: PropTypes.func.isRequired, // Add PropTypes for the click handler
+  onCardClick: PropTypes.func.isRequired,
 };
 
 export default Classes;
