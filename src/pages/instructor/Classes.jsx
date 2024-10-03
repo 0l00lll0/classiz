ba46184e206def506d3d3c8f0a3569a8887847b1
Nicole <qlngncl@gmail.com>
2024-10-03 13:37:51 +0800

    try for home & classes

--- src/pages/instructor/Classes.jsx
+++ src/pages/instructor/Classes.jsx
@@ -1,41 +1,48 @@
-// src/pages/instructor/Classes.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
+import personIcon from '../../media/person-icon.svg'; // Adjust the path as necessary
+import '../../css/Classes.css'; // Ensure this file contains the CSS styles
 
-const Classes = ({ classes }) => {
+const Classes = ({ classes, onCardClick }) => {
   return (
     <div>
       <h2>Class List</h2>
-      <ul>
+      <div className="grid">
         {classes.map((classItem, index) => (
-          <li key={index}>
-            {typeof classItem === 'object' ? (
-              <div>
-                <h3>{classItem.name}</h3>
-                <p>{classItem.description}</p>
-                <p>Type: {classItem.type}</p>
-              </div>
-            ) : (
-              classItem
-            )}
-          </li>
+          <div
+            className="card"
+            key={index}
+            onClick={() => onCardClick(classItem)} // Add click event handler
+          >
+            <div className="card-content">
+              <h3 className="card-title">{classItem.name}</h3>
+              <hr />
+              <p>{classItem.description}</p> {/* Use description instead of code */}
+              <p className="card-text">
+                <img
+                  src={personIcon}
+                  alt="Students Icon"
+                  className="icon-image"
+                />
+                {classItem.enrollment} Students Enrolled
+              </p>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
 
 Classes.propTypes = {
   classes: PropTypes.arrayOf(
-    PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-      }),
-    ])
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired, // Ensure description is required
+      enrollment: PropTypes.number.isRequired,
+    })
   ).isRequired,
+  onCardClick: PropTypes.func.isRequired, // Add PropTypes for the click handler
 };
 
 export default Classes;
--- src/pages/instructor/Home.jsx
+++ src/pages/instructor/Home.jsx
@@ -1,7 +1,84 @@
 import React from 'react';
+import '../../css/Home.css';
+import programmingImage from '../../media/programming.svg';
+import frenchLitImage from '../../media/FrenchLit.svg';
+import historyImage from '../../media/History.svg';
+import chemistryImage from '../../media/Chemistry.svg';
+import dsaImage from '../../media/DSA.svg';
+import personIcon from '../../media/person-icon.svg';
 
 const Home = () => {
-  return <div>Home Page</div>;
-};
+  return (
+    <div className="container">
+      <main className="main-content">
+        <div className="main-card">
+          <h2 className="welcome-title">
+            Welcome to class<span className="colored">iz</span>, <br /> Nikki!
+          </h2>
+        </div>
+
+        <div className="grid">
+          {courses.map((course, index) => (
+            <div className="card" key={index}>
+              <div className="card-image-container">
+                <img
+                  src={course.image}
+                  alt="Course Image"
+                  className="card-image"
+                />
+              </div>
+              <div className="card-content">
+                <h3 className="card-title">{course.title}</h3>
+                <hr />
+                <p>{course.code}</p>
+                <p className="card-text">
+                  <img
+                    src={personIcon}
+                    alt="Students Icon"
+                    className="icon-image"
+                  />
+                  {course.enrollment} Students Enrolled
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+const courses = [
+  {
+    title: 'Programming',
+    code: 'BSCS2002',
+    enrollment: 20,
+    image: programmingImage,
+  },
+  {
+    title: 'French Literature',
+    code: 'BSLIT210',
+    enrollment: 20,
+    image: frenchLitImage,
+  },
+  {
+    title: 'History',
+    code: 'BSH2761',
+    enrollment: 20,
+    image: historyImage,
+  },
+  {
+    title: 'Chemistry',
+    code: 'BSCH2002',
+    enrollment: 20,
+    image: chemistryImage,
+  },
+  {
+    title: 'Data Structures',
+    code: 'BSCS2002',
+    enrollment: 20,
+    image: dsaImage,
+  },
+];
 
 export default Home;
